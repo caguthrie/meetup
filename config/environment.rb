5719696e4aae7ee781aba3f66ac286dbe4c86eac
Christopher Guthrie <christopher.a.guthrie@gmail.com>
2014-03-01 14:02:46 -0500

    env

--- config/environment.rb
+++ config/environment.rb
@@ -4,7 +4,8 @@ Bundler.require(:default)
 
 require_relative '../models/restaurant'
 require_relative '../models/violation'
-require_relative '../spec/migration_spec.rb'
+require_relative '../models/restaurant_violation'
+# require_relative '../spec/migration_spec.rb'
 # require_relative '../app'
 
 ActiveRecord::Base.establish_connection(
