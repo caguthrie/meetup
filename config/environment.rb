508f16bd29ba1ed131fe5cf82cd560c587740b1c
Christopher Guthrie <christopher.a.guthrie@gmail.com>
2014-03-03 18:14:05 -0500

    Removed cuisines from environment

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,6 @@ Bundler.require(:default)
 require_relative '../models/restaurant'
 require_relative '../models/violation'
 require_relative '../models/restaurant_violation'
-require_relative '../models/cuisine'
 require_relative '../spec/restaurant_violation_spec.rb'
 require_relative '../spec/restaurant_spec.rb'
 require_relative '../spec/violation_spec.rb'
