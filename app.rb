4494936ed2bf76c24eed639bac5c24ec5abde901
Christopher Guthrie <christopher.a.guthrie@gmail.com>
2014-03-03 20:15:06 -0500

    Messing with app.rb

--- app.rb
+++ app.rb
@@ -1,39 +1,28 @@
 class RestaurantApp < Sinatra::Base
 
   get '/' do
+    @message = @@message
     erb :index
   end
 
   post '/' do
-    @zip_url = params["zip"].to_i
-    @worst = Restaurant.worst_restaurant_in_zip(@zip_url, 3)
-    if !Restaurant.exists?(zip: @zip_url)
-      @message = "Error, invalid zipcode!"
-      redirect to{"/"}
-    else
-      @message = ""
-      erb :zip_page
-    end
+    post_up(params["zip"])
   end
 
   get '/:zip_url' do
-    @zip_url = params[:zip_url].to_i
+    post_up(params[:zip_url])
+  end
+
+  def post_up(p)
+    @zip_url = p.to_i
     @worst = Restaurant.worst_restaurant_in_zip(@zip_url, 3)
     if !Restaurant.exists?(zip: @zip_url)
-      @message = "Error, invalid zipcode!"
+      @@message = "Error, invalid zipcode!"
       redirect to{"/"}
     else
-      @message = ""
+      @@message = ""
       erb :zip_page
     end
   end
-
-  # post '/' do
-  #   m = Message.new
-  #   m.from = params[:from]
-  #   m.content = params[:content]
-  #   m.save
-  #   "success"
-  # end
 
 end
